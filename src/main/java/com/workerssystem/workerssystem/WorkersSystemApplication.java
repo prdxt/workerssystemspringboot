c936929988c2df0cb9228ed176edfbe7630ef97e
prdxt <dominosjaj@gmail.com>
2023-05-30 14:37:32 +0200

    s

---
+++ src/main/java/com/workerssystem/workerssystem/WorkersSystemApplication.java
@@ -0,0 +1,13 @@
+package com.workerssystem.workerssystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkersSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkersSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/workerssystem/workerssystem/WorkersSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workerssystem.workerssystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkersSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
